31c40abc6e34a12a3fd9ec099d6651194fea4711
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-11-08 17:03:35 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home.js';
+import Login from './Components/Login/Login';
 
 const App = () => {
-    const nome = "nick";
-    const ativo = false;
-    return <div className={ativo ? 'ativo' : 'inativo'}>Meu App {nome}</div>
+
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                    </Routes>
+                <Footer />
+            </BrowserRouter>
+
+        </div>
+    )
 }
 
 export default App;
