ccb680b3be72ff35d35911682bc2c610b34bf938
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-11-09 10:59:24 -0300

    UseContext - usuario

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home.js';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 const App = () => {
 
     return (
         <div>
             <BrowserRouter>
-                <Header />
+                <UserStorage>
+                    <Header />
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/login/*" element={<Login />} />
                     </Routes>
-                <Footer />
+                    <Footer />
+                </UserStorage>
             </BrowserRouter>
 
         </div>
