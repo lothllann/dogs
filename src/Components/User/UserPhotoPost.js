6e52bdd3a71f54a67e75fe8645f90b84aff33dbc
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-11-11 21:06:45 -0300

    photo post

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import styles from './UserPhotoPost.module.css'
+import useForm from '../../Hooks/userForm';
+import Button from '../Form/Button';
+import Error from '../Helper/Error';
+import Input from '../Form/Input';
 
 const UserPhotoPost = () => {
+const nome = useForm();
+const peso = useForm();
+const idade = useForm();
+
+
+  function handleSubmit(event) {
+    event.prevenDefault();
+    
+  }
+
+  function handleImgChange() {
+    
+  }
   return (
-    <div>UserPhotoPost</div>
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="nome" />
+        <Input label="Peso" type="text" name="peso" />
+        <Input label="Idade" type="text" name="idade" />
+        <input type="file" name='img' id='img' onChange={handleImgChange} />
+        <Button>Enviar</Button>
+      </form>
+
+    </section>
   )
 }
 
