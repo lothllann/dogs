7f42a5a5b99a26222c5623ecbea63fc122a04e98
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-12-16 11:31:44 -0300

    Estatisticas

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import { STATS_GET } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 import Head from '../Helper/Head';
+import Loading from '../Helper/Loading';
 
 const UserStats = () => {
-  return (
-    <div>
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options)
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error} />
+  if (data)
+    return (
+      <div>
         <Head title='Estatisticas' />
-    </div>
-  )
+      </div>
+    );
+  else return null;
 }
 
 export default UserStats;
