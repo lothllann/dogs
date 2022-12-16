10b98697eea0af7b558045e6265b54e59c642dd9
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-12-16 11:07:53 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -15,18 +15,20 @@ import { UserStorage } from './UserContext';
 const App = () => {
 
     return (
-        <div>
+        <div className="app">
             <BrowserRouter>
                 <UserStorage>
                     <Header />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/login/*" element={<Login />} />
-                        <Route path="/conta/*" element={<ProtectedRoute> <User /> </ProtectedRoute>} />
-                        <Route path="/foto/:id" element={<Photo />} />
-                        <Route path="/perfil/:user" element={<UserProfile />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
+                    <main className="appBody">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/login/*" element={<Login />} />
+                            <Route path="/conta/*" element={<ProtectedRoute> <User /> </ProtectedRoute>} />
+                            <Route path="/foto/:id" element={<Photo />} />
+                            <Route path="/perfil/:user" element={<UserProfile />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </main>
                     <Footer />
                 </UserStorage>
             </BrowserRouter>
